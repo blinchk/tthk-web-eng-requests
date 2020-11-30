77b43bc7af9092ab6e001399ff405bee8181e830
Opilane TTHK <opilane@tthk.ee>
2020-11-30 10:50:41 +0200

    Add URL for responses.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,14 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(event) {
+    const number = document.querySelector('input[type=number]').value;
+    console.log(number);
+    
+    let url = 'http://api.icndb.com/jokes/random/';
+    fetch(url).then( (response) => {
 
+    }).catch(error => {
+        console.log(error);
+    })
+    event.preventDefault();
 }
